6c4cb9774cb3a8b7ca8e251174cb67fe2ad4aea9
Manoj Kumar K <manojkumark@Manojs-MacBook-Air.local>
2025-03-25 23:41:42 +0530

    test case fixed

--- spec/services/forecast_service_spec.rb
+++ spec/services/forecast_service_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe ForecastService do
   describe '.fetch' do
-    it 'returns forecast data for a valid ZIP code' do
-      result = ForecastService.fetch('10001') # You can change this
+    it 'returns current weather data for a valid location' do
+      result = ForecastService.fetch('10001') # or "London"
 
       expect(result).to be_a(Hash)
       expect(result[:current_temp]).to be_present
-      expect(result[:high]).to be_present
-      expect(result[:low]).to be_present
+      expect(result[:condition]).to be_present
+      expect(result[:icon]).to be_present
     end
 
-    it 'returns nil for an invalid ZIP code' do
+    it 'returns nil for an invalid location' do
       result = ForecastService.fetch('invalid_location_123')
       expect(result).to be_nil
     end
