ad1f353597ff18f5e9915d32a71b3fa2e3217413
Manoj Kumar K <manojkumark@Manojs-MacBook-Air.local>
2025-09-20 21:01:45 +0530

    sample

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,8 @@ class UsersController < ApplicationController
 
   private
 
+
+  
   def user_params
     params.require(:user).permit(:name, :email, :phone, :password, :password_confirmation)
   end
